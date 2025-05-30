78f822bbe52b7a68517ceb91d55d470f207431b8
yangsejin <yangsejin@yangsejin-ui-MacBookAir.local>
2025-05-30 18:27:43 +0900

    다시 정상화

--- server.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = "mongodb+srv://yangsejin811:23243298y@cluster0.s1abc.mongodb.net/commentsDB?retryWrites=true&w=majority";
-
-mongoose.connect(uri)
-  .then(() => console.log('MongoDB 연결 성공'))
-  .catch(err => console.error('MongoDB 연결 오류:', err));
